0227992ee4896baef313baafe408caa2f41efbe8
esoft32 - Marina Azevedo <marina.mafs@gmail.com>
2021-10-14 19:57:24 -0300

    cleanup

--- src/delete.js
+++ src/delete.js
@@ -4,25 +4,22 @@ const TelegramBaseController = Telegram.TelegramBaseController
 
 class DeleteController extends TelegramBaseController {
     deleteHandler($) {
-        
-        $.runMenu({
-            message: 'Proceder para deletar seus dados? Essa operação não pode ser desfeita:',
-            layout: 2,
-            oneTimeKeyboard: true,
-            'SIM': () => {
+            $.runMenu({
+                message: 'Proceder para deletar seus dados? Essa operação não pode ser desfeita:',
+                layout: 2,
                 oneTimeKeyboard: true,
-
-                $.sendMessage(`Conta deletada!`)
-            },
-            'NÃO': () => {
+                'SIM': () => {
+                    oneTimeKeyboard: true,
+                    $.sendMessage(`Conta deletada!`)
+                },
+                'NÃO': () => {
+                    oneTimeKeyboard: true,
+                    $.sendMessage(`Bom te ter aqui!`)
+                },
+            'anyMatch': () => {
                 oneTimeKeyboard: true,
-
-                $.sendMessage(`Bom te ter aqui!`)
-            },
-        'anyMatch': () => { //will be executed at any other message
-            oneTimeKeyboard: true,
-            $.sendMessage('Message')
-        }
+                $.sendMessage('Message')
+            }
         })
     }
 
